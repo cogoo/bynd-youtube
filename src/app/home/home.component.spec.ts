0a866ad33c2a3dbb944896e6fff3806fc883d665
Colin Ogoo <ogoo.colin@gmail.com>
2018-07-15 09:35:16 +0100

    add test

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -24,8 +24,8 @@ describe('HomeComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
 
