2cd23d420fb4274e6bf064d825e7da775b58c5d4
Colin Ogoo <ogoo.colin@gmail.com>
2018-07-16 12:59:10 +0100

    add rendertron image

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -2,13 +2,13 @@ import { MetaService } from './../service/meta.service';
 import { YoutubeService } from './../service/youtube.service';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { of } from 'rxjs';
-import { DebugElement } from '../../../node_modules/@angular/core';
+import { DebugElement } from '@angular/core';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
